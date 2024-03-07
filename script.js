a91aca12f5d589f45801f01bc0a7378c957307e4
Michael Yoo <michaelyoo18@gwu.edu>
2024-03-07 17:33:11 -0500

    finishing up

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
   var modal = document.getElementById('projectModal');
   var closeModal = document.getElementsByClassName('close')[0];
   var descriptionButtons = document.querySelectorAll('.description-btn');
+  function toggleArrowVisibility(shouldHide) {
+    var arrows = document.querySelectorAll('.slider-arrow');
+    arrows.forEach(function(arrow) {
+      arrow.style.display = shouldHide ? 'none' : 'block';
+    });
+  }
   descriptionButtons.forEach(btn => {
     btn.addEventListener('click', function() {
       var title = this.closest('.details-container').querySelector('.project-title').textContent;
@@ -19,16 +25,32 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('modalDescription').innerHTML = `<ul>${descriptionList}</ul>`;
       
       modal.style.display = 'block';
+      toggleArrowVisibility(true);
     });
   });
   
   closeModal.onclick = function() {
     modal.style.display = 'none';
+    toggleArrowVisibility(false);
   }
 
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = 'none';
+      toggleArrowVisibility(false);
     }
   }
 });
+
+function toggleProjects() {
+  var firstSet = document.getElementById('first-set-of-projects');
+  var secondSet = document.getElementById('second-set-of-projects');
+
+  if (firstSet.style.display === "block" || firstSet.style.display === "") {
+    firstSet.style.display = "none";
+    secondSet.style.display = "block";
+  } else {
+    firstSet.style.display = "block";
+    secondSet.style.display = "none";
+  }
+}
