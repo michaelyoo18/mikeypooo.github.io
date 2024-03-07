f5dea31012d320af4ad29e37a92bc8c28f99c2c2
Michael Yoo <111733324+myoo18@users.noreply.github.com>
2024-03-07 02:22:09 -0500

    fixing typo/cleaning code

--- script.js
+++ script.js
@@ -6,37 +6,26 @@ function toggleMenu() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Get the modal
   var modal = document.getElementById('projectModal');
-  
-  // Get the <span> element that closes the modal
   var closeModal = document.getElementsByClassName('close')[0];
-  
-  // Get all buttons that open the modal
   var descriptionButtons = document.querySelectorAll('.description-btn');
-
-  // Function to open modal and populate with project info
   descriptionButtons.forEach(btn => {
     btn.addEventListener('click', function() {
       var title = this.closest('.details-container').querySelector('.project-title').textContent;
-      var descriptionText = this.getAttribute('data-description'); // Get the description from data attribute
-      var descriptionList = descriptionText.split('||').map(item => `<li>${item}</li>`).join(''); // Create list items
+      var descriptionText = this.getAttribute('data-description');
+      var descriptionList = descriptionText.split('||').map(item => `<li>${item}</li>`).join('');
       
       document.getElementById('modalTitle').textContent = title;
-      document.getElementById('modalDescription').innerHTML = `<ul>${descriptionList}</ul>`; // Use innerHTML to render the list
+      document.getElementById('modalDescription').innerHTML = `<ul>${descriptionList}</ul>`;
       
       modal.style.display = 'block';
     });
   });
   
-  
-
-  // When the user clicks on <span> (x), close the modal
   closeModal.onclick = function() {
     modal.style.display = 'none';
   }
 
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = 'none';
